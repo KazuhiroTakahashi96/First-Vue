cc4cd0a8d37c68a50335b6cdeb069afe0606b879
Kazu Takahashi <num96kazu19@icloud.com>
2022-06-10 00:05:30 +0900

    first vue-cli, vue-router, vue ui エラーの大群。。。

---
+++ first-vue-cli/first-cli/src/views/HomeStudy.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Study</h2>
+  <p>I don't like studying. And you???</p>
+</template>
+
+<style scoped>
+  h2 {
+    font-size: 100px;
+  }
+
+  p {
+    font-size: 50px;
+  }
+</style>
