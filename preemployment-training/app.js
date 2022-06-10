567de38896ec4d525e6fcfcd0e949331957210ac
Kazu Takahashi <num96kazu19@icloud.com>
2022-06-10 18:55:14 +0900

    npm init vue@latest でコマンド実行したvueプロジェクト

--- preemployment-training/app.js
+++ preemployment-training/app.js
@@ -1,4 +1,13 @@
 // https://router.vuejs.org/guide/#javascript
+// https://reffect.co.jp/vue/first-time-vue-router#google_vignette
+
+// 1.ログインページにユーザー名とパスワードの入力項目を設定
+// 2.入力チェックをvee-validateで実装
+// ユーザー名入力必須
+// パスワード6字以上入力必須
+// 3.ログインボタンでページに遷移できるよう実装
+
+import Login from "./component/login";
 
 const Home = { template: "<h3>This is Home page</h3>" };
 const Works = { template: "<h3>This is Works page.</h3>" };
@@ -8,6 +17,7 @@ const routes = [
   { path: "/", component: Home },
   { path: "/works", component: Works },
   { path: "/about", component: About },
+  { path: "/login", component: Login },
 ];
 
 const router = VueRouter.createRouter({
@@ -17,19 +27,19 @@ const router = VueRouter.createRouter({
 });
 
 const app = Vue.createApp({
-  data() {
-    return {
-      text: "Hello World!!!",
-      lists: ["JS", "TS", "React", "Vue", "Angular", "Nuxt", "Next"],
-      counter: 1,
-      input: "",
-    };
-  },
-  methods: {
-    click() {
-      this.counter++;
-    },
-  },
+  // data() {
+  //   return {
+  //     text: "Hello World!!!",
+  //     lists: ["JS", "TS", "React", "Vue", "Angular", "Nuxt", "Next"],
+  //     counter: 1,
+  //     input: "",
+  //   };
+  // },
+  // methods: {
+  //   click() {
+  //     this.counter++;
+  //   },
+  // },
 });
 
 app.use(router);
---
+++ preemployment-training/component/login.vue
@@ -0,0 +1,30 @@
+<script>
+import { Form, Field } from "vee-validate";
+
+export default {
+  components: {
+    Field,
+    Form,
+    ErrorMessage,
+  },
+  methods: {
+    isRequired(value) {
+      if (value && value.trim()) {
+        return true;
+      }
+      return "This is required";
+    },
+  },
+};
+</script>
+
+<template>
+  <Form>
+    <label for="">ユーザー名</label>
+    <Field type="text" name="field" :rules="isRequired" />
+    <label for="">パスワード</label>
+    <Field type="text" name="password" type="password" />
+  </Form>
+</template>
+
+<style></style>
